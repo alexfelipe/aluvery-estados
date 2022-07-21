781cfa8a9746b13638d9c3710f6ccf3e73cc4362
Alex Felipe <alexfelipevieira@gmail.com>
2022-07-21 08:45:40 -0300

    2.3

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,41 +1,34 @@
 package br.com.alura.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.alura.aluvery.R
 import br.com.alura.aluvery.model.MappedProducts
-import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.sampleMappedProducts
-import br.com.alura.aluvery.sampledata.sampleProducts
 import br.com.alura.aluvery.ui.components.ProductsSection
-import java.math.BigDecimal
 
 @Composable
 fun HomeScreen(
-    mappedProducts: List<MappedProducts>
+    sections: List<MappedProducts>
 ) {
-    Column(
+    LazyColumn(
         Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(vertical = 16.dp)
     ) {
-        Spacer(Modifier)
-        for (mappedProduct in mappedProducts) {
-            ProductsSection(
-                title = mappedProduct.title,
-                products = mappedProduct.products
+        items(sections) { section ->
+            ProductsSection(x
+                title = section.title,
+                products = section.products
             )
         }
-        Spacer(Modifier)
     }
 }
 

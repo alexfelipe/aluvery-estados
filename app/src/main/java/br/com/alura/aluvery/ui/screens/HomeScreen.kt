39b66cbc71d2ab597f239fae22f8fba9f2c5d30f
Alex Felipe <alexfelipevieira@gmail.com>
2022-07-21 15:02:32 -0300

    3.1

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,10 +1,13 @@
 package br.com.alura.aluvery.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,17 +20,22 @@ import br.com.alura.aluvery.ui.components.ProductsSection
 fun HomeScreen(
     sections: List<MappedProducts>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        items(sections) { section ->
-            ProductsSection(x
-                title = section.title,
-                products = section.products
-            )
+    Column {
+        TextField(value = "alex", onValueChange = {
+            Log.i("HomeScreen", "textField: $it")
+        })
+        LazyColumn(
+                Modifier
+                        .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            items(sections) { section ->
+                ProductsSection(
+                        title = section . title,
+                        products = section.products
+                )
+            }
         }
     }
 }

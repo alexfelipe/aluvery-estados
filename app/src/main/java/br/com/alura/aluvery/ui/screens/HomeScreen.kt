4269dbe96ffdd5fd7f7c1db64cbe135e9a276350
Alex Felipe <alexfelipevieira@gmail.com>
2022-07-22 09:00:57 -0300

    3.2

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -9,20 +9,29 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.model.MappedProducts
 import br.com.alura.aluvery.sampledata.sampleMappedProducts
 import br.com.alura.aluvery.ui.components.ProductsSection
+import kotlin.random.Random
 
 @Composable
 fun HomeScreen(
     sections: List<MappedProducts>
 ) {
     Column {
-        TextField(value = "alex", onValueChange = {
-            Log.i("HomeScreen", "textField: $it")
+        var state: MutableState<String> = remember {
+            mutableStateOf("${Random.nextInt()}")
+        }
+        TextField(value = state.value, onValueChange = { newValue ->
+            Log.i("HomeScreen", "textField: $newValue")
+            Log.i("HomeScreen", "value: ${state.value}")
+            state.value = newValue
         })
         LazyColumn(
                 Modifier

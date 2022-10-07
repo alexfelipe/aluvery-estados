f21281e937b2ca29888f2c758e5a657902766c04
Alex Felipe <alexfelipevieira@gmail.com>
2022-10-07 08:56:30 -0300

    1.3

--- app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
@@ -3,8 +3,13 @@ package br.com.alura.aluvery.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 class ProductFormActivity : ComponentActivity() {
@@ -14,10 +19,56 @@ class ProductFormActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    Text(text = "Formulário de produto")
+                    ProductFormScreen()
                 }
             }
         }
     }
 
+}
+
+@Composable
+fun ProductFormScreen() {
+    Column {
+        Text(text = "Criando o produto")
+        var url by remember {
+            mutableStateOf("")
+        }
+        TextField(value = url, onValueChange = {
+            url = it
+        })
+
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        })
+        var price by remember {
+            mutableStateOf("")
+        }
+        TextField(value = price, onValueChange = {
+            price = it
+        })
+
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(value = description, onValueChange = {
+            description = it
+        })
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Salvar")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProductFormScreenPreview() {
+    AluveryTheme {
+        Surface {
+            ProductFormScreen()
+        }
+    }
 }

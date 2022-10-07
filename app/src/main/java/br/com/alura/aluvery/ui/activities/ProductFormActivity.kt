c7eef54f8c02917414af8ec3668577a7fe8d3ed8
Alex Felipe <alexfelipevieira@gmail.com>
2022-10-07 10:35:00 -0300

    2.2

--- app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
@@ -5,17 +5,23 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.alura.aluvery.R
 import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.ui.theme.AluveryTheme
+import coil.compose.AsyncImage
 import java.math.BigDecimal
 
 class ProductFormActivity : ComponentActivity() {
@@ -38,9 +44,11 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
+        Spacer(modifier = Modifier)
         Text(
             text = "Criando o produto",
             Modifier.fillMaxWidth(),
@@ -48,6 +56,17 @@ fun ProductFormScreen() {
         )
         var url by remember {
             mutableStateOf("")
+        }
+        if(url.isNotBlank()) {
+            AsyncImage(
+                model = url, contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
         }
         TextField(
             value = url,
@@ -119,6 +138,7 @@ fun ProductFormScreen() {
         ) {
             Text(text = "Salvar")
         }
+        Spacer(modifier = Modifier)
     }
 }
 

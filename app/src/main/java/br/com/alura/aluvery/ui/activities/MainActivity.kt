99777e3a78f66197d769bceafe7314b64843bc7a
Alex Felipe <alexfelipevieira@gmail.com>
2022-10-10 09:10:12 -0300

    4.1

--- app/src/main/java/br/com/alura/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/activities/MainActivity.kt
@@ -1,10 +1,20 @@
 package br.com.alura.aluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.screens.HomeScreen
 import br.com.alura.aluvery.ui.theme.AluveryTheme
@@ -13,18 +23,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App(onFabClick = {
+                startActivity(Intent(
+                    this,
+                    ProductFormActivity::class.java
+                ))
+            })
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(onFabClick: () -> Unit = {}) {
     AluveryTheme {
         Surface {
-            HomeScreen(
-                sampleSections
-            )
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = onFabClick) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    HomeScreen(
+                        sampleSections
+                    )
+                }
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
 }

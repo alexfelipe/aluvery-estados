05bad3e43d110d6627ec0cad90da6ddd80984375
Alex Felipe <alexfelipevieira@gmail.com>
2022-10-28 10:28:44 -0300

    5.1

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,13 +1,12 @@
 package br.com.alura.aluvery.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,7 +17,6 @@ import br.com.alura.aluvery.ui.components.ProductsSection
 import br.com.alura.aluvery.ui.components.SearchTextField
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 import br.com.alura.aluvery.ui.viewmodels.HomeScreenViewModel
-import kotlin.random.Random
 
 data class HomeScreenUiState(
     val sections: Map<String, List<Product>> = emptyMap(),
@@ -37,7 +35,7 @@ data class HomeScreenUiState(
 fun HomeScreen(
     viewModel: HomeScreenViewModel
 ) {
-    val state = viewModel.uiState
+    val state by viewModel.uiState.collectAsState()
     HomeScreen(state = state)
 }
 
--- app/src/main/java/br/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -1,9 +1,5 @@
 package br.com.alura.aluvery.ui.viewmodels
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.alura.aluvery.dao.ProductDao
@@ -12,30 +8,33 @@ import br.com.alura.aluvery.sampledata.sampleCandies
 import br.com.alura.aluvery.sampledata.sampleDrinks
 import br.com.alura.aluvery.sampledata.sampleProducts
 import br.com.alura.aluvery.ui.screens.HomeScreenUiState
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.math.BigDecimal
 
 class HomeScreenViewModel : ViewModel() {
 
     private val dao = ProductDao()
 
-    var uiState: HomeScreenUiState by mutableStateOf(
-        HomeScreenUiState(
-            onSearchChange = {
-                val searchedProducts = searchedProducts(it)
-                uiState = uiState.copy(
-                    searchText = it,
-                    searchedProducts = searchedProducts
-                )
-            }
-        ))
-        private set
+    private val _uiState = MutableStateFlow(HomeScreenUiState())
+    val uiState get() = _uiState.asStateFlow()
 
     init {
+        _uiState.update { state ->
+            state.copy(
+                onSearchChange = {
+                    val searchedProducts = searchedProducts(it)
+                    _uiState.value = _uiState.value.copy(
+                        searchText = it,
+                        searchedProducts = searchedProducts
+                    )
+                }
+            )
+        }
         viewModelScope.launch {
             dao.products.collect { products ->
-                uiState = uiState.copy(
+                _uiState.value = _uiState.value.copy(
                     sections = mapOf(
                         "Todos produtos" to products,
                         "Promoções" to sampleDrinks + sampleCandies,
